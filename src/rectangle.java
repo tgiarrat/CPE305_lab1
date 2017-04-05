efeb51d01713f04cfcd70ad3407f423e4673717c
tgiarrat <tgiarrat@calpoly.edu>
2017-04-05 12:19:03 -0700

    more setup

---
+++ src/rectangle.java
@@ -0,0 +1,34 @@
+
+public class rectangle implements shape {
+
+	@Override
+	public int compute_area() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int compute_center_mass() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void scale(int factor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void rotate(int num_degrees) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
