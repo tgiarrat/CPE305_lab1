bcdd2c771298621cfaef2509014bae996af16b0f
tgiarrat <tgiarrat@calpoly.edu>
2017-04-05 11:38:48 -0700

    initial commit

---
+++ src/main.java
@@ -0,0 +1,19 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/**
+		 * a) instantiate 3 shapes: one circle (center (0,0), radius 5), one rectangle (lower left corner (3,3), width 5, height 8), one triangle (one corner (-2,0), 2nd corner (2,0), 3rd corner (0,4))
+
+b) generate text output of the 3 shapes
+
+c) scale the circle by 2, rotate the rectangle by 45 degrees, scale the triangle by 3
+
+d) generate text output of the 3 modified shapes
+		 */
+		
+	}
+
+}
