a09faa6fa288eaba0f94fe6eef6e550c6cdd9466
tgiarrat <tgiarrat@calpoly.edu>
2017-04-05 19:03:25 -0700

    last change

---
+++ src/RectangleModifier.java
@@ -0,0 +1,14 @@
+
+public class RectangleModifier implements ShapeModifier {
+
+	@Override
+	public void scale(Shape shape, int factor) {
+
+	}
+
+	@Override
+	public void rotate(int num_degrees) {
+		System.out.println("Rotate the rectangle " + num_degrees);
+	}
+
+}
